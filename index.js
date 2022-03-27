796578ae449ad342c9246d65326b1c81bb2def41
Timothy Addison <git@tjaddison.com>
2022-03-27 16:21:49 -0400

    extract ensure token method

--- index.js
+++ index.js
@@ -1,35 +1,13 @@
-import * as readline from "node:readline/promises";
-import { stdin as input, stdout as output } from "node:process";
-import { getRequestToken, getAccessToken, getArticles } from "./pocketApi.js";
-import { getSecretsFromFile, updateSecretsFile } from "./util.js";
+import { ensureAccessTokenIsValid, getArticles } from "./pocketApi.js";
+import { getSecretsFromFile } from "./util.js";
 
 const constants = {
-  RedirectUri: "http://localhost:3000",
   SecretsFileName: "secrets.json",
 };
 
 let { AccessToken, ConsumerKey } = await getSecretsFromFile(constants.SecretsFileName);
 
-// TODO: If there is an access token, check it is still valid
-
-// If there is no access token, get one and store it in the secrets file
-if (!AccessToken) {
-  const requestToken = await getRequestToken(
-    ConsumerKey,
-    constants.RedirectUri
-  );
-
-  console.log(
-    `Please visit https://getpocket.com/auth/authorize?request_token=${requestToken}&redirect_uri=${constants.RedirectUri}`
-  );
-  const rl = readline.createInterface({ input, output });
-  await rl.question("Press enter once you have authorized the application\r\n");
-  rl.close();
-
-  AccessToken = await getAccessToken(requestToken, ConsumerKey);
-
-  await updateSecretsFile(constants.SecretsFileName, AccessToken, ConsumerKey);
-}
+AccessToken = await ensureAccessTokenIsValid(AccessToken, ConsumerKey, constants.SecretsFileName);
 
 const { articleList, newSince } = await getArticles(AccessToken, ConsumerKey);
 console.log({ articles: Object.keys(articleList).length, newSince });
--- pocketApi.js
+++ pocketApi.js
@@ -1,3 +1,7 @@
+import { updateSecretsFile } from "./util.js";
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
 const endpoints = {
   GetRequestToken: "https://getpocket.com/v3/oauth/request",
   GetAccessToken: "https://getpocket.com/v3/oauth/authorize",
@@ -9,24 +13,26 @@ const headers = {
   "X-Accept": "application/json",
 };
 
+const redirect_uri = "http://localhost:3000";
+
 // https://getpocket.com/developer/docs/authentication
 // https://getpocket.com/developer/docs/v3/retrieve
 
 // The request token is also called a code in the pocket authentication APIs
-export const getRequestToken = async function (consumerKey, redirectUri) {
+const getRequestToken = async function (consumerKey) {
   const response = await fetch(endpoints.GetRequestToken, {
     method: "POST",
     headers,
     body: JSON.stringify({
       consumer_key: consumerKey,
-      redirect_uri: redirectUri,
+      redirect_uri,
     }),
   });
   const responseJson = await response.json();
   return responseJson.code;
 };
 
-export const getAccessToken = async function (requestToken, consumerKey) {
+const getAccessToken = async function (requestToken, consumerKey) {
   const response = await fetch(endpoints.GetAccessToken, {
     method: "POST",
     headers,
@@ -39,7 +45,11 @@ export const getAccessToken = async function (requestToken, consumerKey) {
   return responseJson.access_token;
 };
 
-export const getArticles = async function (accessToken, consumerKey, since = 0) {
+export const getArticles = async function (
+  accessToken,
+  consumerKey,
+  since = 0
+) {
   const response = await fetch(endpoints.GetArticles, {
     method: "POST",
     headers,
@@ -57,4 +67,33 @@ export const getArticles = async function (accessToken, consumerKey, since = 0) 
   const { list: articleList, since: newSince } = responseJson;
 
   return { articleList, newSince };
+};
+
+export const ensureAccessTokenIsValid = async function (
+  existingAccessToken,
+  ConsumerKey,
+  secretsFileName
+) {
+  // TODO: If there is an access token, check it is still valid
+
+  // If there is no access token, get one and store it in the secrets file
+  if (!existingAccessToken) {
+    const requestToken = await getRequestToken(ConsumerKey);
+
+    console.log(
+      `Please visit https://getpocket.com/auth/authorize?request_token=${requestToken}&redirect_uri=${redirect_uri}`
+    );
+    const rl = readline.createInterface({ input, output });
+    await rl.question(
+      "Press enter once you have authorized the application\r\n"
+    );
+    rl.close();
+
+    const newAccessToken = await getAccessToken(requestToken, ConsumerKey);
+    
+    await updateSecretsFile(secretsFileName, newAccessToken, ConsumerKey);
+    return newAccessToken;
+  }
+
+  return existingAccessToken;
 };
