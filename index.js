357c56294f95dec99d93b2992d29f55b915ab1f4
Timothy Addison <git@tjaddison.com>
2022-04-02 20:12:59 -0400

    bugfix: look at key count (articlelist is not an array)

--- index.js
+++ index.js
@@ -11,10 +11,15 @@ const { articleList, newSince } = await getArticles(
   Since
 );
 
-if (!articleList.length) {
+// articleList is an object where each saved article is a property
+// where the key is the article ID
+const articleCount = Object.keys(articleList).length;
+
+if (articleCount === 0) {
   console.log("No articles to write");
 } else {
   await writeArticlesToFile(articleList);
+  console.log(`Wrote ${articleCount} articles`);
 }
 
 if (newSince > Since) {
